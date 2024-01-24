3549f68a164326223cfd32827a4b3e8a4dbabcda
Norwil <46763365+Norwil@users.noreply.github.com>
2024-01-25 00:44:33 +0100

    Create Task10.java

---
+++ Week1/Task10.java
@@ -0,0 +1,13 @@
+/* 10. What is the longest local name you can use in a Java program on your system? What is the longest external name? What are the restrictions on the characters that can appear in names. 
+
+Answer similar questions about integers and real numbers. 
+
+
+In Java:
+
+Longest Name: No strict limit; practical limit based on tools and readability.
+Character Restrictions: Start with letter/underscore/dollar sign; then letters, digits, underscores, or dollar signs. Case-sensitive.
+Longest External Name: No strict limit; practical limit based on file system and readability.
+
+
+  */
