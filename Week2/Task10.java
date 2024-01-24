6583642d88cf17fab1fd462ecd83b983982aa1df
Norwil <46763365+Norwil@users.noreply.github.com>
2024-01-25 00:51:46 +0100

    Create Task10.java

---
+++ Week2/Task10.java
@@ -0,0 +1,9 @@
+/*  Task 4: Print Alphabet Letters Using a Loop */
+
+public class PrintAlphabet {
+    public static void main(String[] args) {
+        for (char letter = 'A'; letter <= 'Z'; letter++) {
+            System.out.print(letter + " ");
+        }
+    }
+}
