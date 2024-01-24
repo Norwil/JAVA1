5852394ee06c0014b4bb1425750063219fccd031
Norwil <46763365+Norwil@users.noreply.github.com>
2024-01-25 00:46:21 +0100

    Create Task1.java

---
+++ Week2/Task1.java
@@ -0,0 +1,32 @@
+/* Task 1: Check if a Number is Prime*/
+
+import java.util.Scanner;
+
+public class PrimeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num == 2) {
+            return true;
+        }
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
