8a439c025af9399118a007171068c4dcbc0270bc
Norwil <46763365+Norwil@users.noreply.github.com>
2024-01-25 00:38:24 +0100

    Create Task2.java

---
+++ Week1/Task2.java
@@ -0,0 +1,17 @@
+"""2. Create two int variables named z1 and z2, with values 30 and 6 respectively, and then print various arithmetic operations."""
+
+
+public class Main {
+    public static void main(String[] args) {
+        int z1 = 30;
+        int z2 = 6;
+
+        // Perform and print arithmetic operations
+        System.out.println(z1 + " + " + z2 + " = " + (z1 + z2));
+        System.out.println(z1 + " - " + z2 + " = " + (z1 - z2));
+        System.out.println(z1 + " * " + z2 + " = " + (z1 * z2));
+        System.out.println(z1 + " / " + z2 + " = " + (z1 / z2));
+        System.out.println(z1 + " % " + z2 + " = " + (z1 % z2));
+        System.out.println("31 / 6 = " + (31 / 6));
+    }
+}
