102799eead053fe7c539496666cbd83e6aaba30a
Norwil <46763365+Norwil@users.noreply.github.com>
2024-01-25 00:37:42 +0100

    Create Task1.java

---
+++ Week1/Task1.java
@@ -0,0 +1,8 @@
+"""1. Write and run a program that outputs the text "Hey everyone" to the screen. """
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hey everyone");
+    }
+}
+
