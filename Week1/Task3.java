82585dbcb4dd7f895bb2eaa815b8b5de67362aff
Norwil <46763365+Norwil@users.noreply.github.com>
2024-01-25 00:39:04 +0100

    Create Task3.java

---
+++ Week1/Task3.java
@@ -0,0 +1,11 @@
+"""Create a char variable with the name letter and the value 'a'. Then increment and print the value of this variable on the screen."""
+
+public class Main {
+    public static void main(String[] args) {
+        char letter = 'a';
+
+        // Increment and print the character
+        letter++;
+        System.out.println("Incremented letter: " + letter);
+    }
+}
