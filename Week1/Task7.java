96b5078dc680c51dc681708f6f3680a73786326b
Norwil <46763365+Norwil@users.noreply.github.com>
2024-01-25 00:41:51 +0100

    Create Task7.java

---
+++ Week1/Task7.java
@@ -0,0 +1,9 @@
+/*Print the integer digits from 1 to 10 using a for and then a while loop. What other types of loops does Java have? Solve this task using them.*/
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
